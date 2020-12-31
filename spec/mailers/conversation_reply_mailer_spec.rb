d65eaecc3cf9578ed3a59f2f082170c1865c2378
Furkan metin OĞUZ <dumstp1@gmail.com>
2020-12-31 11:13:08 +0300

    terms cond

--- spec/mailers/conversation_reply_mailer_spec.rb
+++ spec/mailers/conversation_reply_mailer_spec.rb
@@ -115,7 +115,7 @@ RSpec.describe ConversationReplyMailer, type: :mailer do
     end
 
     context 'when inbox email address is available' do
-      let(:inbox) { create(:inbox, account: account, email_address: 'noreply@chatwoot.com') }
+      let(:inbox) { create(:inbox, account: account, email_address: 'info@mirror.com.tr') }
       let(:conversation) { create(:conversation, assignee: agent, inbox: inbox, account: account) }
       let!(:message) { create(:message, conversation: conversation, account: account) }
       let(:mail) { described_class.reply_with_summary(message.conversation, Time.zone.now).deliver_now }
